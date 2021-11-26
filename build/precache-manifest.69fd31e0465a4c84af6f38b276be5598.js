0bf56da5dcc6e7b7575cf58b95cc30927f28b713
nathias <n@th1.as>
2021-11-26 17:45:50 +0100

    Fix warnings

---
+++ build/precache-manifest.69fd31e0465a4c84af6f38b276be5598.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88ac6d9bbcba03f31449adf80915fa52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07e00e4786a0c29d7c8d",
+    "url": "/static/css/main.0e632e19.chunk.css"
+  },
+  {
+    "revision": "c23d5974e2945618490f",
+    "url": "/static/js/2.89a5c1db.chunk.js"
+  },
+  {
+    "revision": "07e00e4786a0c29d7c8d",
+    "url": "/static/js/main.645ae738.chunk.js"
+  },
+  {
+    "revision": "f67c199efb9f2c29f389",
+    "url": "/static/js/runtime-main.f294effb.js"
+  }
+]);
